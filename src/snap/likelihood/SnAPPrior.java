c70d907370056c3822c429da4ee63316450de4df
davidjamesbryant <davidjamesbryant@users.noreply.github.com>
2010-07-26 04:52:53 +0000

    short message

--- src/snap/likelihood/SnAPPrior.java
+++ src/snap/likelihood/SnAPPrior.java
@@ -30,6 +30,7 @@ package snap.likelihood;
 
 
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -42,96 +43,128 @@ import beast.evolution.tree.Tree;
 import beast.evolution.tree.Node;
 
 @Description("Standard prior for SnAP analysis, consisting of a Yule prior on the tree " +
-		"(parameterized by lambda) " +
-		"and gamma distribution over the theta values " +
-		"(with parameters alpha and beta). " +
-		"Thetas are represented by the gamma parameter where values are theta=2/gamma")
+        "(parameterized by lambda) " +
+        "and gamma distribution over the theta values " +
+        "(with parameters alpha and beta). " +
+        "Thetas are represented by the gamma parameter where values are theta=2/gamma")
 public class SnAPPrior extends Distribution {
 //	public Input<Parameter> m_pU = new Input<Parameter>("mutationRateU", "mutation rate from red to green?");
-//	public Input<Parameter> m_pV = new Input<Parameter>("mutationRateV", "mutation rate from green to red?");
-	public Input<RealParameter> m_pAlpha = new Input<RealParameter>("alpha", "prior parameter -- see docs for details");
-	public Input<RealParameter> m_pBeta = new Input<RealParameter>("beta", "prior parameter -- see docs for details");
-	public Input<RealParameter> m_pLambda = new Input<RealParameter>("lambda", "parameter for Yule birth process");
-	public Input<RealParameter> m_pGamma = new Input<RealParameter>("gamma", "Populations sizes for the nodes in the tree");
-	public Input<Tree> m_pTree = new Input<Tree>("tree", "tree with phylogenetic relations");
-	
-	public SnAPPrior() {
-	}
-	
-	public void initAndValidate() {
-	}
-	
+    //	public Input<Parameter> m_pV = new Input<Parameter>("mutationRateV", "mutation rate from green to red?");
+    public Input<RealParameter> m_pAlpha = new Input<RealParameter>("alpha", "prior parameter -- see docs for details");
+    public Input<RealParameter> m_pBeta = new Input<RealParameter>("beta", "prior parameter -- see docs for details");
+    public Input<RealParameter> m_pLambda = new Input<RealParameter>("lambda", "parameter for Yule birth process");
+    public Input<RealParameter> m_pGamma = new Input<RealParameter>("gamma", "Populations sizes for the nodes in the tree");
+    public Input<Tree> m_pTree = new Input<Tree>("tree", "tree with phylogenetic relations");
 
-	@Override
-	public double calculateLogP() throws Exception {
-		logP = 0.0;
+    public SnAPPrior() {
+    }
 
-		//Mutation rates
-		//	uniform pi_0 on [0,1]. Rate equals one.
+    public void initAndValidate() {
+    }
+
+
+    @Override
+    public double calculateLogP() throws Exception {
+        logP = 0.0;
+
+        //Mutation rates
+        //	uniform pi_0 on [0,1]. Rate equals one.
 //		if (state.getValue(m_pU) < 0.0 || state.getValue(m_pV) < 0.0 )
 //			return Double.NEGATIVE_INFINITY; //zero prior probability.
 
 
-		//branch lengths / tree height
-		//Assume a yule prior with given birthrate lambda.
-		//computeHeights(state.tree);
-		//state.m_trees[m_nTreeID].calculateHeightsFromLengths();
-		Tree tree = (Tree) m_pTree.get();
-		double heightsum = tree.getRoot().getHeight(); 
-		heightsum += heightSum(tree.getRoot()); 
+        //branch lengths / tree height
+        //Assume a yule prior with given birthrate lambda.
+        //computeHeights(state.tree);
+        //state.m_trees[m_nTreeID].calculateHeightsFromLengths();
+        Tree tree = (Tree) m_pTree.get();
+        double heightsum = tree.getRoot().getHeight();
+        heightsum += heightSum(tree.getRoot());
 
-		int nspecies = (tree.getNodeCount() + 1) / 2;
-		double lambda = m_pLambda.get().getValue();
-		double alpha = m_pAlpha.get().getValue();
-		double beta = m_pBeta.get().getValue();
-		// note: nspecies-2, not nspecies-1
-		// SHOULD BE -1 not -2?!?
-		logP += (nspecies-1)*Math.log(lambda) - lambda*heightsum;
+        int nspecies = (tree.getNodeCount() + 1) / 2;
+        double lambda = m_pLambda.get().getValue();
+        double alpha = m_pAlpha.get().getValue();
+        double beta = m_pBeta.get().getValue();
 
-		//Gamma values in tree
-		RealParameter gamma = m_pGamma.get();
-		//double [] gamma = state.getParameter(m_pGamma).getValues();
-		//	We assume that theta has a gamma (alpha,beta) distribution, so that
-		//the gamma parameter has 2/gamma(alpha,beta) distribution
-		for (int iNode = 0; iNode < gamma.getDimension(); iNode++) {
-			double x = 2.0/gamma.getValue(iNode);
-			logP += (alpha - 1.0)*Math.log(x) - (beta * x);
-		}
-		return logP;
-	} // calculateLogLikelihood
+        double mu = 0.0; //Death process
+        //Yule Model
 
-	double heightSum(Node node) throws Exception {
-		if (node.isLeaf()) {
-			return 0;
-		} else {
-			double h = node.getHeight(); 
-			h +=
-				heightSum(node.m_left) +
-				heightSum(node.m_right);
-			return h;
-		}
-	} // heightSum
+        if (mu==0.0) {
+            logP += (nspecies-1)*Math.log(lambda) - lambda*heightsum;
+        }
+        else {
+            //Birth death model.    See Thompson 1975, pg 56
+            List<Double> allHeights = getSortedHeights(tree.getRoot());
+            Iterator p = allHeights.iterator();
+            double x1 = (Double)p.next();
+            double p0n= p0(x1,lambda,mu);
+
+            for(int n=2; n<nspecies;n++) {
+                double xn = (Double)p.next();
+                logP += Math.log(mu*p1(xn,lambda,mu)/p0n);
+            }
+
+        }
+        //Gamma values in tree
+        RealParameter gamma = m_pGamma.get();
+        //double [] gamma = state.getParameter(m_pGamma).getValues();
+        //	We assume that theta has a gamma (alpha,beta) distribution, so that
+        //the gamma parameter has 2/gamma(alpha,beta) distribution
+        for (int iNode = 0; iNode < gamma.getDimension(); iNode++) {
+            double x = 2.0/gamma.getValue(iNode);
+            logP += (alpha - 1.0)*Math.log(x) - (beta * x);
+        }
+        return logP;
+    } // calculateLogLikelihood
+
+    double heightSum(Node node) throws Exception {
+        if (node.isLeaf()) {
+            return 0;
+        } else {
+            double h = node.getHeight();
+            h +=
+                    heightSum(node.m_left) +
+                            heightSum(node.m_right);
+            return h;
+        }
+    } // heightSum
+
+    //Returns a list of branching times in the tree, sorted in an decreasing sequence. First one is
+    //the height of the mrca of the tree.
+    List<Double> getSortedHeights(Node node) throws Exception {
+        return null;
+    }
 
 
-	@Override
-	public List<String> getArguments() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+    private double p0(double t, double lambda, double mu) {
+        return mu*(1-Math.exp(-(lambda-mu)*t))/(lambda - mu*Math.exp(-(lambda-mu)*t));
+    }
+
+    private double p1(double t, double lambda, double mu) {
+        double denominator = (lambda - mu*Math.exp(-(lambda-mu)*t));
+        return (lambda - mu)*(lambda - mu) * Math.exp(-(lambda-mu)*t) / (denominator * denominator);
+    }
+
+    @Override
+    public List<String> getArguments() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 
-	@Override
-	public List<String> getConditions() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<String> getConditions() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 
-	@Override
-	public void sample(State state, Random random) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void sample(State state, Random random) {
+        // TODO Auto-generated method stub
 
-	
+    }
+
+
 } // class SSSPrior 
