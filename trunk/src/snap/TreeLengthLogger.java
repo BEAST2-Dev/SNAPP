d3cf6c84578ed8309b9eee818efad0636dd8b732
higgs.ml <higgs.ml@d103ca59-3e82-fb19-257b-5fe16e790019>
2010-09-08 03:40:55 +0000

    a beast II internal burp

--- trunk/src/snap/TreeLengthLogger.java
+++ trunk/src/snap/TreeLengthLogger.java
@@ -11,9 +11,9 @@ import beast.core.parameter.RealParameter;
 import beast.evolution.tree.Node;
 import beast.evolution.tree.Tree;
 
-@Description("Logger to report sum of branch lengths of a tree")
+@Description("Logger to report sum of node heights of a tree")
 public class TreeLengthLogger extends Plugin implements Loggable {
-	public Input<Tree> m_tree = new Input<Tree>("tree", "tree to report sum of branch lengths for.", Validate.REQUIRED);
+	public Input<Tree> m_tree = new Input<Tree>("tree", "tree to report on.", Validate.REQUIRED);
 
 	@Override
 	public void initAndValidate() {
