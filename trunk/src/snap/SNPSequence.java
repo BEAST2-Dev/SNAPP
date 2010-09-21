f1031fa3e4fb4f3732249b05e25c72404fda02f5
higgs.ml <higgs.ml@d103ca59-3e82-fb19-257b-5fe16e790019>
2010-09-21 01:55:00 +0000

    support for binary sequences

---
+++ trunk/src/snap/SNPSequence.java
@@ -0,0 +1,50 @@
+package snap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.evolution.alignment.Sequence;
+
+@Description("A SNPSequence holds a collection of binary sequences that are summarized in a new sequence " +
+		"by counting the number of sites with a 1.")
+public class SNPSequence extends Sequence {
+	public Input<List<Sequence>> m_sequences = new Input<List<Sequence>>("sequence","binary sequence", new ArrayList<Sequence>());
+	
+	@Override
+	public void initAndValidate() throws Exception {
+		m_nTotalCount.setValue(m_sequences.get().size(), this);
+	}
+
+	@Override
+	public List<Integer> getSequence(String sDataMap) throws Exception {
+        Integer [] sequences = null;
+        
+        // grab info from sub sequences and add them up
+        for (Sequence sequence: m_sequences.get()) {
+        	if (sequences == null) {
+        		sequences = sequence.getSequence(sDataMap).toArray(new Integer[0]);
+        	} else {
+                List<Integer> sequence2 = sequence.getSequence(sDataMap);
+                // sanity check: make sure sequence2 is of same length as rest
+                if (sequence2.size() != sequences.length) {
+                	throw new Exception("sequence " + sequence.m_sTaxon.get() + " is of length " + sequence2.size() + 
+                			" but it was expected to be of length " + sequences.length);
+                }
+                for (int i = 0; i < sequences.length; i++) {
+                	sequences[i] += sequence2.get(i);
+                }
+        	}
+        }
+        
+        // convert array to list
+        List<Integer> list = new ArrayList<Integer>();
+        for (Integer i : sequences) {
+        	list.add(i);
+        }
+        return list;
+	}
+	
+	
+} // class SNPSequence
