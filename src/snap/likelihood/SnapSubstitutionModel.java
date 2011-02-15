5948a158252a8cc6feccabfaa6e66a59418b380b
higgs.ml <rbouckaert@users.noreply.github.com>
2011-02-15 20:47:12 +0000

    beast2 housekeeping

--- src/snap/likelihood/SnapSubstitutionModel.java
+++ src/snap/likelihood/SnapSubstitutionModel.java
@@ -1,7 +1,7 @@
 package snap.likelihood;
 
-import beast.core.CalculationNode;
 import beast.core.Input;
+import beast.core.Input.Validate;
 import beast.core.parameter.RealParameter;
 import beast.evolution.substitutionmodel.EigenDecomposition;
 import beast.evolution.substitutionmodel.SubstitutionModel;
@@ -11,6 +11,10 @@ public class SnapSubstitutionModel extends SubstitutionModel.Base {
 	public Input<RealParameter> m_pU = new Input<RealParameter>("mutationRateU", "mutation rate from red to green?");
 	public Input<RealParameter> m_pV = new Input<RealParameter>("mutationRateV", "mutation rate from green to red?");
 	public Input<RealParameter> m_pCoalescenceRate = new Input<RealParameter>("coalescenceRate", "population size parameter with one value for each node in the tree");
+	
+	public SnapSubstitutionModel() {
+		frequencies.setRule(Validate.OPTIONAL);
+	}
 	
     @Override
     public void initAndValidate() {}
