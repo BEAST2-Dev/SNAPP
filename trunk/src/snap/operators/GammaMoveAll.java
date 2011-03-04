57b063a47ee2ba61f2bea95a1b27ee8fd17ba600
davidjamesbryant <davidjamesbryant@d103ca59-3e82-fb19-257b-5fe16e790019>
2011-03-04 01:24:27 +0000

    added operator moving all gammas simultaneous (probably redundant)

---
+++ trunk/src/snap/operators/GammaMoveAll.java
@@ -0,0 +1,68 @@
+
+/*
+ * File GammaMover.java
+ *
+ * Copyright (C) 2010 Remco Bouckaert, David Bryant remco@cs.auckland.ac.nz
+ *
+ * This file is part of SnAP.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * SnAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  SnAP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SnAP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+package snap.operators;
+
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Operator;
+import beast.core.parameter.RealParameter;
+import beast.util.Randomizer;
+
+@Description("Scales single value in gamma parameter.")
+public class GammaMoveAll extends Operator {
+	public Input<RealParameter> m_coalescenceRate = new Input<RealParameter>("coalescenceRate", "population sizes");
+	public Input<Double> m_pScale = new Input<Double>("scale", "scale of move");
+
+	double m_fScale;
+
+	@Override
+	public void initAndValidate() {
+		m_fScale = m_pScale.get();
+	}
+	
+	@Override
+	public double proposal() {
+		RealParameter coalescenceRate = m_coalescenceRate.get(this);
+		double scale = Math.exp(m_fScale*(2.0*Randomizer.nextDouble() - 1.0));
+		int numNodes = coalescenceRate.getDimension();
+		
+		for(int whichNode=0;whichNode<numNodes;whichNode++) 
+			coalescenceRate.setValue(whichNode, coalescenceRate.getValue(whichNode)*scale);
+		
+		
+		return numNodes * Math.log(scale);
+	}
+ 
+	/** automatic parameter tuning **/
+	@Override
+	public void optimize(double logAlpha) {
+		Double fDelta = calcDelta(logAlpha);
+		fDelta += Math.log(m_fScale);
+		m_fScale = Math.exp(fDelta);
+    }
+	
+} // class GammaMover 
