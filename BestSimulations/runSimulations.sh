266e2498e7dab0b73f47376bf608db8aa5e58dc5
davidjamesbryant <davidjamesbryant@users.noreply.github.com>
2011-03-12 16:02:12 +0000

    Scripts for simulation

--- BestSimulations/runSimulations.sh
+++ BestSimulations/runSimulations.sh
@@ -9,8 +9,8 @@ SIMSNAP="./simsnap"
 HARDTREE4="(((A[theta=0.006]:0.0057,B[theta=0.006]:0.0057)[theta=0.008]:0.0005,C[theta=0.006]:0.0062)[theta=0.005]:0.0078,D[theta=0.006]:0.014)[theta=0.006]:0.0;"
 EASYTREE4="(((A[theta=0.006]:0.0057,B[theta=0.006]:0.0057)[theta=0.005]:0.0045,C[theta=0.006]:0.0102)[theta=0.005]:0.0138,D[theta=0.006]:0.024)[theta=0.006]:0.0;"
 
-HARDTREE8="((((A[theta=0.006]:0.005,B[theta=0.006]:0.005)[theta=0.008]:0.0026,C[theta=0.006]:0.0076)[theta=0.009]:0.0004, D[theta=0.006]:0.008)[theta=0.005]:.010,((E[theta=0.006]:0.003,F[theta=0.006]:0.003)[theta=0.001]:0.004,(G[theta=0.006]:0.0068,H[theta=0.006]:0.0068)[theta=0.014]:0.0002)[theta=0.001]:0.011)[theta=0.011]:0.0;"
-EASYTREE8="((((A[theta=0.006]:0.003,B[theta=0.006]:0.003)[theta=0.005]:0.0056,C[theta=0.006]:0.0086)[theta=0.004]:0.0054, D[theta=0.004]:0.014)[theta=0.002]:.004,((E[theta=0.006]:0.003,F[theta=0.006]:0.003)[theta=0.001]:0.009,(G[theta=0.006]:0.0048,H[theta=0.006]:0.0048)[theta=0.004]:0.0072)[theta=0.001]:0.006)[theta=0.021]:0.0;"
+HARDTREE8='((((A[theta=0.006]:0.005,B[theta=0.006]:0.005)[theta=0.008]:0.0026,C[theta=0.006]:0.0076)[theta=0.009]:0.0004,D[theta=0.006]:0.008)[theta=0.005]:.010,((E[theta=0.006]:0.003,F[theta=0.006]:0.003)[theta=0.001]:0.004,(G[theta=0.006]:0.0068,H[theta=0.006]:0.0068)[theta=0.014]:0.0002)[theta=0.001]:0.011)[theta=0.011]:0.0;'
+EASYTREE8='((((A[theta=0.006]:0.003,B[theta=0.006]:0.003)[theta=0.005]:0.0056,C[theta=0.006]:0.0086)[theta=0.004]:0.0054,D[theta=0.004]:0.014)[theta=0.002]:.004,((E[theta=0.006]:0.003,F[theta=0.006]:0.003)[theta=0.001]:0.009,(G[theta=0.006]:0.0048,H[theta=0.006]:0.0048)[theta=0.004]:0.0072)[theta=0.001]:0.006)[theta=0.021]:0.0;'
 
 TAXANAMES=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
@@ -80,16 +80,21 @@ function runSnapSingleSim {
 	cd $TMP
 	
 	echo 'Running SNAP on '$1' in tmp directory '$TMP
-	
+#	cat $1
+
 	#Run snap
-	java -jar $HERE/snap.jar  -overwrite -seed $2 $1 &>snapjunk
-	
+	java -jar $HERE/snap.jar  -threads 4 -overwrite -seed $2 $1 &>snapjunk
+#	echo finished snap
+#	ls
+#	cat snapjunk
+
 	#Rename output files
 	mv *.log $FILEROOT'_seed'$2'.log'
 	mv *.trees $FILEROOT'_seed'$2'.trees'
 	
 	java -cp $HERE/snap.jar  snap.util.TreeSetAnalyser3 -tree $3 *.trees 2> $FILEROOT'_seed'$2'_results.txt' 1>&-
-	
+#	echo finished post analysis
+#	ls
 	#Back to the original directory
 	tar -czf $HERE/$FILEROOT'_seed'$2.tgz $FILEROOT'_seed'$2.log $FILEROOT'_seed'$2'.trees' $1
         
@@ -121,35 +126,52 @@ function runSNAPandDelete {
 # Size of credibility set
 # Y or N indicating if true is in credibility set.
 
+function testTopology {
+#arguments.  1: number of taxa. 2: tree. 3: runName 4: Chain length
 
 
-makeSimSnapInput thetree.txt 4 1 1 $EASYTREE4 
+    makeSimSnapInput thetree.txt $1 1 1 $2
+ 
+cat thetree.txt
+
+
 for nsites in 100 200 300 400 500 600
 do
 	$SIMSNAP -s $nsites thetree.txt 1> original_$nsites'.txt' 2>&-
 
-	rm thetree.txt
+
 	
 #2x2 combinations of prior.
 
-	cp thetree_tree_1.xml 'tree_'$nsites'_aa.xml'
-	modifySnapXML 'tree_'$nsites'_aa.xml' 1 200 50 200000
-	cp thetree_tree_1.xml 'tree_'$nsites'_ab.xml'
-	modifySnapXML 'tree_'$nsites'_ab.xml' 1 200 25 200000
-	cp thetree_tree_1.xml 'tree_'$nsites'_ba.xml'
-	modifySnapXML 'tree_'$nsites'_ba.xml' 1 2000 50 200000
-	cp thetree_tree_1.xml 'tree_'$nsites'_bb.xml'
-	modifySnapXML 'tree_'$nsites'_bb.xml' 1 2000 25 200000
-	
-	rm thetree_tree_1.xml
 
-	for filename in 'tree_'$nsites'_aa.xml' 'tree_'$nsites'_ab.xml' 'tree_'$nsites'_ba.xml' 'tree_'$nsites'_bb.xml'
+	basename='tree_'$3'_'$nsites
+
+
+	cp thetree_tree_1.xml $basename'_aa.xml'
+	modifySnapXML $basename'_aa.xml' 1 200 50 $4
+	cp thetree_tree_1.xml $basename'_ab.xml'
+	modifySnapXML $basename'_ab.xml' 1 200 25 $4
+	cp thetree_tree_1.xml $basename'_ba.xml'
+	modifySnapXML $basename'_ba.xml' 1 2000 50 $4
+	cp thetree_tree_1.xml $basename'_bb.xml'
+	modifySnapXML $basename'_bb.xml' 1 2000 25 $4
+
+	for filename in $basename'_aa.xml'   $basename'_ab.xml' $basename'_ba.xml' $basename'_bb.xml' 
 	do
-		 	 runSNAPandDelete $filename 200 $EASYTREE4  &
+	    runSNAPandDelete $filename 200 $2  &
       	done
-
+	wait
 
 done
+	rm thetree.txt
+}
+
+chainLength=200000
+testTopology 4 $EASYTREE4 "easy4" $chainLength
+testTopology 4 $HARDTREE4 "hard4" $chainLength
+testTopology 8 $EASYTREE8 "easy8" $chainLength
+testTopology 8 $HARDTREE8 "hard8" $chainLength
+
 
 
 
