d0b5575080f22ad0d7abbd3311722dafcb87fd7d
higgs.ml@gmail.com <higgs.ml@gmail.com@d103ca59-3e82-fb19-257b-5fe16e790019>
2012-06-04 07:45:47 +0000

    add 'balanced' flag to WeightedData to allow balancing of patterns, hence fixing mutation rates to u=v=1

--- trunk/src/snap/WeightedData.java
+++ trunk/src/snap/WeightedData.java
@@ -5,10 +5,15 @@ import java.util.Arrays;
 import beast.core.Description;
 import beast.core.Input;
 import beast.core.Input.Validate;
+import beast.util.Randomizer;
 
 @Description("Alignment with weights attached to patterns")
 public class WeightedData extends Data {
 	public Input<String> weightInput = new Input<String>("weights", "comma separated list of weights, one for each site in the alignment", Validate.REQUIRED);
+	// balance
+	public Input<Boolean> balance = new Input<Boolean>("balanced", "whether to attempt to reassign labels so #reds approx equal to #greens. " +
+			"This assumes there is no significance in colour of the labellings. " +
+			"Patterns and complementary patterns are found and weights swapped", false);
 
 	@Override
 	public void initAndValidate() throws Exception {
@@ -25,6 +30,81 @@ public class WeightedData extends Data {
 			int userWeight = Integer.parseInt(sStr[i]);;
 			m_nWeight[getPatternIndex(i)] += userWeight;
 		}
+		if (balance.get()) {
+			attemptToBalance();
+		}
 	} // initAndValidate
+
+	private void attemptToBalance() {
+		int nPatterns = m_nPatterns.length-2;
+		int [] complement = new int[nPatterns];
+		Arrays.fill(complement, -1);
+
+		// find complement of each pattern
+		for (int i = 0; i < nPatterns; i++) {
+			int [] pattern0 = getPattern(i);
+			for (int j = i+1; j < nPatterns; j++) {
+				int [] pattern1 = getPattern(j);
+				boolean match = true;
+				for (int k = 0; k < pattern0.length; k++) {
+					if (pattern0[k] + pattern1[k] != m_nStateCounts.get(k)) {
+						match = false;
+						break;
+					}
+				}
+				if (match) {
+					complement[i] = j;
+					complement[j] = i;
+				}
+			}
+		}
+
+		// determine contribution of each pattern
+		int [] patternscore = new int[nPatterns];
+		int [] score = new int[nPatterns];
+		int currentScore = 0;
+		for (int i = 0; i < nPatterns; i++) {
+			int [] pattern0 = getPattern(i);
+			for (int k = 0; k < pattern0.length; k++) {
+				patternscore[i] += pattern0[k];
+			}
+			score[i] = patternscore[i] * m_nWeight[i];
+			currentScore += score[i]; 
+		}	
+
+		// calc target score
+		int targetScore = 0;
+		int totalLineages = 0;
+		for (int k = 0; k < getPattern(0).length; k++) {
+			totalLineages += m_nStateCounts.get(k);
+		}
+		for (int i = 0; i < nPatterns; i++) {
+			targetScore += totalLineages * m_nWeight[i];
+		}
+		targetScore = targetScore / 2;
+		
+		// swap weights
+		int delta = targetScore - currentScore;
+		int attempts = 0;
+		while (delta != 0 && attempts < 10 * nPatterns) {
+			int i = Randomizer.nextInt(nPatterns);
+			if (complement[i] >= 0) {
+				int j = complement[i];
+				int newScore = currentScore - m_nWeight[i] * patternscore[i] - m_nWeight[j] * patternscore[j] +
+						m_nWeight[i] * patternscore[j] + m_nWeight[j] * patternscore[i];
+				int newDelta = targetScore - newScore;
+				if (Math.abs(newDelta) < Math.abs(delta)) {
+					int tmp = m_nWeight[i];
+					m_nWeight[i] = m_nWeight[j];
+					m_nWeight[j] = tmp;
+					delta = newDelta;
+					currentScore = newScore;
+				}
+			}
+			attempts++;
+		}
+		
+		System.out.println(Arrays.toString(m_nWeight));
+	} // attemptToBalance
 	
 }
