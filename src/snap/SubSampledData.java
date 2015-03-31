e2c4b28c2ee2e89580a08c89394304a31dd9aa44
rbouckaert <higgs.ml@gmail.com>
2015-04-01 12:08:32 +1300

    debugging subsampler

--- src/snap/SubSampledData.java
+++ src/snap/SubSampledData.java
@@ -7,7 +7,6 @@ import beast.core.Description;
 import beast.core.Input;
 import beast.core.Input.Validate;
 import beast.evolution.alignment.TaxonSet;
-import beast.util.Randomizer;
 
 @Description("Alignment where within species lineages are sampled in order to reduce the dataset")
 public class SubSampledData extends Data {
@@ -17,12 +16,16 @@ public class SubSampledData extends Data {
 	
 	@Override
 	public void initAndValidate() throws Exception {
-		List<TaxonSet> taxa = m_taxonsets.get();
+		Data data = dataInput.get();
+		m_dataType = data.getDataType();
+		List<TaxonSet> taxa = data.m_taxonsets.get();
 		List<TaxonSet> subtaxa = new ArrayList<>();
 		
 		double sampleProportion = proportionInput.get();
 		int minLineages = minLineagesInput.get();
 		
+		List<List<Integer>> datacounts = data.getCounts();
+
 		for (int i = 0; i < taxa.size(); i++) {
 			TaxonSet orgSet = taxa.get(i);
 			if (minLineages > orgSet.getTaxonCount()) {
@@ -31,26 +34,39 @@ public class SubSampledData extends Data {
 			}
 			
 			TaxonSet subset = new TaxonSet();
-			subset.setID(orgSet.getID()+ "subset");
+			String seqid = orgSet.getID()+ "subset";
+			subset.setID(seqid);
 			
-			
-			boolean [] done = new boolean[orgSet.getTaxonCount()];
-			for (int j = 0; j < Math.max(minLineages, sampleProportion * orgSet.getTaxonCount()); j++) {
-				int k = Randomizer.nextInt(orgSet.getTaxonCount());
-				while (done[k]) {
-					k = Randomizer.nextInt(orgSet.getTaxonCount());
-				}
-				subset.taxonsetInput.setValue(orgSet.getTaxonId(k), subset);
-				done[k] = true;
+
+			// select sampleProportion fraction of lineages without replacement
+			List<Integer> seq = new ArrayList<Integer>();
+			SNPSequence s = new SNPSequence();
+			s.taxonInput.setValue(seqid, s);
+			int totalCount = (int) Math.max(minLineages, sampleProportion * orgSet.getTaxonCount()+ 0.5);
+			s.totalCountInput.setValue(totalCount, s);
+			double fraction = Math.max((double)minLineages / orgSet.getTaxonCount(), sampleProportion);
+			for (int j = 0; j < datacounts.get(i).size(); j++) {
+				seq.add((int)(datacounts.get(i).get(j) * fraction + 0.5));
 			}
+			counts.add(seq);
+			sequenceInput.setValue(s, this);
+//			boolean [] done = new boolean[orgSet.getTaxonCount()];
+//			for (int j = 0; j < Math.max(minLineages, sampleProportion * orgSet.getTaxonCount() + 0.5); j++) {
+//				int k = Randomizer.nextInt(orgSet.getTaxonCount());
+//				while (done[k]) {
+//					k = Randomizer.nextInt(orgSet.getTaxonCount());
+//				}
+//				subset.taxonsetInput.setValue(orgSet.getTaxonId(k), subset);
+//				done[k] = true;
+//			}
 			subtaxa.add(subset);
 		}
 		
 		m_taxonsets.get().clear();
 		m_taxonsets.get().addAll(subtaxa);
-		
-		super.initAndValidate();
+		calcPatterns();
+		sequenceInput.get().clear();
 	}
 
-}
+} // class SubSampledData
 
