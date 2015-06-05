629867a12e2416e8f083f0b2737e93873bbe8e0d
davidjamesbryant <david.bryant@otago.ac.nz>
2015-06-05 16:20:00 +1200

    Added Quadrature Rule

--- src/snap/likelihood/BetaApproximationLikelihood.java
+++ src/snap/likelihood/BetaApproximationLikelihood.java
@@ -2,6 +2,8 @@ package snap.likelihood;
 
 
 
+import java.util.Arrays;
+
 import snap.Data;
 import beast.core.Citation;
 import beast.core.Description;
@@ -23,158 +25,338 @@ public class BetaApproximationLikelihood extends GenericTreeLikelihood {
 	public BetaApproximationLikelihood() {
 		siteModelInput.setRule(Validate.OPTIONAL);
 	}
-	
+
 	Tree tree;
 	Double rootTheta; //Parameter for root distribution. 
 	Data data;	
-	
+
 	int nPoints;    //Size of mesh used in integration
 	int thisPattern; 
 	protected double[][] partialIntegral;  //Partial likelihoods. First index = node, second index = point number.
 	protected int[] lineageCounts; //Lineage counts for each leaf taxa, for current pattern.
 	protected int[] redCounts; //Red allele counts for each leaf, for current pattern.
 	protected double[] patternLogLikelihoods;
-	
-    /**
-     * flag to indicate the
-     * // when CLEAN=0, nothing needs to be recalculated for the node
-     * // when DIRTY=1 indicates a node partial needs to be recalculated
-     * // when FILTHY=2 indicates the indices for the node need to be recalculated
-     * // (often not necessary while node partial recalculation is required)
-     */
-    protected int hasDirt;
 
-    /**
-     * Lengths of the branches in the tree associated with each of the nodes
-     * in the tree through their node  numbers. By comparing whether the
-     * current branch length differs from stored branch lengths, it is tested
-     * whether a node is dirty and needs to be recomputed (there may be other
-     * reasons as well...).
-     * These lengths take branch rate models in account.
-     */
-    protected double[] m_branchLengths;
-    protected double[] storedBranchLengths;
-    
-    RealParameter coalescenceRate;
+	/**
+	 * flag to indicate the
+	 * // when CLEAN=0, nothing needs to be recalculated for the node
+	 * // when DIRTY=1 indicates a node partial needs to be recalculated
+	 * // when FILTHY=2 indicates the indices for the node need to be recalculated
+	 * // (often not necessary while node partial recalculation is required)
+	 */
+	protected int hasDirt;
 
-	
+	/**
+	 * Lengths of the branches in the tree associated with each of the nodes
+	 * in the tree through their node  numbers. By comparing whether the
+	 * current branch length differs from stored branch lengths, it is tested
+	 * whether a node is dirty and needs to be recomputed (there may be other
+	 * reasons as well...).
+	 * These lengths take branch rate models in account.
+	 */
+	protected double[] m_branchLengths;
+	protected double[] storedBranchLengths;
+
+	RealParameter coalescenceRate;
+
+
 	@Override
 	public void initAndValidate() throws Exception {
 		tree = (Tree) treeInput.get();
 		data = (Data) dataInput.get();
 		patternLogLikelihoods = new double[data.getPatternCount()];
 		coalescenceRate = coalescenceRateInput.get();
+
+		nPoints = 50; //We need to add a new parameter here. Note that nPoints higher -> more accurate, so we could make
+						//approximations by keeping npoints small.
+		int nNodes = tree.getNodeCount();
+		partialIntegral = new double[nPoints][nNodes];
+
+		testSolve();
 		
-        nPoints = data.getTaxonCount();//20; //TODO: read this from the xml
-        int nNodes = tree.getNodeCount();
-        partialIntegral = new double[nPoints][nNodes];
-        
-        hasDirt = Tree.IS_FILTHY;
+		hasDirt = Tree.IS_FILTHY;
 	}
-	
+
 
 	/**
-     * Calculate the log likelihood of the current state.
-     *
-     * @return the log likelihood.
-     */
-    @Override
-    public double calculateLogP() {
-        rootTheta = coalescenceRate.getValue(tree.getRoot().getNr()); //We should get this from the PARAMETERS.
-    	
-    	double logL=0.0;
-    	
-        for (int thisPattern = 0; thisPattern < data.getPatternCount(); thisPattern++) {
-        	redCounts = data.getPattern(thisPattern);
-        	lineageCounts = data.getPatternLineagCounts(thisPattern);
-        	traverse(tree.getRoot());    //Compute likelihood and puts value in patternLogLikelihood[thisPattern]
-        	
-            logL += patternLogLikelihoods[thisPattern] * data.getPatternWeight(thisPattern);   
-        }
-        return logL;
-    }
-	
-   
-    
-    /**
-     * Evaluate a Simpson's type rule on the interval x[0]...x[n-1], where f[i] = f(x[i])
-     * @param x  array of x values. Assumed to be in strictly increasing order, but need not be regular.
-     * @param f  array of f values with same length as x. 
-     * @return  Simpson's estimate of integral of f between x[0] and x[n-1]. 
-     * If the x[i] are regular and n is even then this returns the standard Simpson's estimate. Otherwise, it 
-     * divides the intervals into consecutive pairs. For each pair  [a,c][c,b] it determines the integral
-     * which is exact for all quadratics (as in Simpson's).
-     * If n is odd then the last interval is evaluated using the the same rule but to evalute the
-     * last integral using the final three points.
-     *     
-     */
-    double simpsons(double[] x, double[] f) {
-    	int n = x.length;
-    	double total = 0.0;
-    	for (int i = 0;i<n-2;i=i+2) {
-    		double a,b,c,wa,wb,wc;
-    		a = x[i]; c = x[i+2]; b = x[i+1]; 
-    		wa = (b-a)*(2.0*a + b - 3.0*c)/(c-a)/6.0;
-    		wb = (b-a)*(a + 2.0*b - 3.0*c)/(c-b)/6.0;
-    		wc = (b-a)*(a-b)*(a-b)/(b-c)/(c-a)/6.0;
-    		total = total + f[i]*wa + f[i+1]*wc+f[i+2]*wb;
-    	}
-    	if (n%2!=0){
-    		double a,b,c,wa,wb,wc;
-    		a = x[n-2]; c = x[n-3]; b = x[n-1]; 
-    		wa = (b-a)*(2.0*a + b - 3.0*c)/(c-a)/6.0;
-    		wb = (b-a)*(a + 2.0*b - 3.0*c)/(c-b)/6.0;
-    		wc = (b-a)*(a-b)*(a-b)/(b-c)/(c-a)/6.0;
-    		total = total + f[n-2]*wa + f[n-3]*wc+f[n-1]*wb;
-    	}
-    	return total;
-    }
-    
-    
-    
-    int traverse(final Node node) {
-        int update = (node.isDirty() | hasDirt| Tree.IS_DIRTY);
+	 * Calculate the log likelihood of the current state.
+	 *
+	 * @return the log likelihood.
+	 */
+	@Override
+	public double calculateLogP() {
+		rootTheta = coalescenceRate.getValue(tree.getRoot().getNr()); //We should get this from the PARAMETERS.
 
-        final int iNode = node.getNr();
-       
+		System.err.println("Do we get here?");
+		double logL=0.0;
 
-        // If the node is internal, update the partial likelihoods.
-        if (!node.isLeaf()) {
+		for (int thisPattern = 0; thisPattern < data.getPatternCount(); thisPattern++) {
+			redCounts = data.getPattern(thisPattern);
+			lineageCounts = data.getPatternLineagCounts(thisPattern);
+			traverse(tree.getRoot());    //Compute likelihood and puts value in patternLogLikelihood[thisPattern]
 
-            // Traverse down the two child nodes
-            final Node child1 = node.getLeft(); //Two children
-            final int update1 = traverse(child1);
+			logL += patternLogLikelihoods[thisPattern] * data.getPatternWeight(thisPattern);   
+		}
+		return logL;
+	}
 
-            final Node child2 = node.getRight();
-            final int update2 = traverse(child2);
+	/**
+	 * Solves Ax = b where A is square and assumed non-singular.
+	 * Uses LU decomposition: Algorithm 3.4.1 of Golub and Van Loan edition 4.
+	 * @param A
+	 * @param b
+	 * @return x
+	 */
 
-            // If either child node was updated then update this node too
-            if (update1 != Tree.IS_CLEAN || update2 != Tree.IS_CLEAN) {
+	private double[] solveMatrixEqn(double[][] A, double[] b) {  	
+		int n = b.length;
+		int[] piv = new int[n];
 
-                final int childNum1 = child1.getNr();
-                final int childNum2 = child2.getNr();
+		for (int k=1;k<n;k++) {
+			int mu = k;
+			double rowMax = Math.abs(A[mu-1][k-1]);
+			for (int i=k+1;i<=n;i++) {
+				double Aik = Math.abs(A[i-1][k-1]);
+				if (Aik > rowMax) {
+					mu = i;
+					rowMax = Aik;
+				}
+			}
+			piv[k-1] = mu;	
+			for (int i=1;i<=n;i++) {
+				double x = A[k-1][i-1];
+				A[k-1][i-1] = A[mu-1][i-1];
+				A[mu-1][i-1] = x;
+			}
+			if (A[k-1][k-1] != 0.0) {
+				for (int rho=k+1;rho<=n;rho++) {
+					A[rho-1][k-1] = A[rho-1][k-1]/A[k-1][k-1];
+					for(int rho2 = k+1;rho2<=n;rho2++) {
+						A[rho-1][rho2-1] = A[rho-1][rho2-1] - A[rho-1][k-1]*A[k-1][rho2-1];
+					}
+				}
+			}
+		}
 
-                update |= (update1 | update2);
+		//Overwrite b by Pb
+		for (int k=1;k<n;k++) {
+			double x = b[k-1];
+			b[k-1] = b[piv[k-1]-1];
+			b[piv[k-1]-1] = x;
+		}
 
-                calculatePartials(childNum1, childNum2, node);
-                
-                if (node.isRoot()) {     
-                	double l = 0.0;
-                	for (int s=0;s<nPoints;s++)
-                		l += partialIntegral[iNode][s];
-                	patternLogLikelihoods[thisPattern] = Math.log(l);              	
-                }
+		double[] x = new double[n];
+		double[] z = new double[n];
+		//solve Lz = Pb, (note - could put x and z in same memory)
+		for (int i=1;i<=n;i++) {
+			z[i-1] = b[i-1];
+			for (int j=1;j<i;j++)
+				z[i-1] = z[i-1] - A[i-1][j-1] * z[j-1];
+		}
+			
+		//solve Ux = z
+		for (int i=n;i>=1;i--) {
+			x[i-1] = z[i-1];
+			for (int j=i+1;j<=n;j++)
+				x[i-1] -= A[i-1][j-1]*x[j-1];
+			x[i-1] = x[i-1]/A[i-1][i-1];	
+		}
+		
+		return x;
 
-            }
-        } else {
-        	//Get the data for this node, and compute the partial likelihoods.
-        	calculateLeafPartials(node);
-        }
-        return update;
-    }
+	}
 
 
-	
+	private void testSolve() {
+		double[][]A = {{3,17,10},{2,4,-2},{6,18,-12}};
+		double[] b = {1,3,4};
+		double[] x = solveMatrixEqn(A,b);
+		for (int i=0;i<3;i++)
+			System.out.println(x[i]);
+	}
+
+	/**
+	 * Evaluates \int x^(alpha-1) f(x)  dx from X[0] ... X[n] by interpolating
+	 * an nth degree polynomial.
+	 * @param X
+	 * @param fx
+	 * @return
+	 */
+	private double alphaKernelQuadrature(double[] X, double[] fx, double alpha) {
+		int N = X.length-1;
+		double a = X[0], b = X[N];
+
+		//z[k] =  \int_a^b x^(alpha-1) x^k dx 
+		double[] z = new double[N+1];
+		for (int k=0;k<N+1;k++)
+			z[k] = (Math.pow(b, alpha+k) - Math.pow(a, alpha+k))/(alpha+k);
+
+		double[][] M = new double[N+1][N+1];
+		for (int k=0;k<N+1;k++)
+			for(int i=0;i<N+1;i++)
+				M[k][i] = Math.pow(X[i],k);
+
+		double[] w = solveMatrixEqn(M,z);
+		double total = 0.0;
+		for (int k = 0;k<N+1;k++)
+			total += fx[k]*w[k];
+		return total;
+	}
+
+
+	/**
+	 * Evaluates \int (1-x)^(beta-1) f(x)  dx from X[0] ... X[n] by interpolating
+	 * an nth degree polynomial.
+	 * @param X
+	 * @param fx
+	 * @return
+	 */
+	private double betaKernelQuadrature(double[] X, double[] fx, double beta) {
+		int N = X.length-1;
+		double a = X[0], b = X[N];
+
+		//z[k] =  \int_a^b x^(alpha-1) x^k dx 
+		double[] z = new double[N+1];
+		for (int k=0;k<N+1;k++)
+			z[k] = (Math.pow((1-a), beta+k) - Math.pow(1-b, beta+k))/(beta+k);
+
+		double[][] M = new double[N+1][N+1];
+		for (int k=0;k<N+1;k++)
+			for(int i=0;i<N+1;i++)
+				M[k][i] = Math.pow(1.0-X[i],k);
+
+		double[] w = solveMatrixEqn(M,z);
+		double total = 0.0;
+		for (int k = 0;k<N+1;k++)
+			total += fx[k]*w[k];
+		return total;
+	}
+
+	/**
+	 * Given points X = [X0,...,XN], where X0=0, XN=1, and function evaluations F[i] = f(X[i])
+	 * estimates integral
+	 *    1/Beta(alpha,beta) \int x^(alpha-1) (1-x)^(beta-1) f(x) dx
+	 * Uses interpolatory quadrature, on sets of 8 points (or thereabouts).
+	 * For the first part of the integral, uses quadrature rule of the form
+	 * int x^(alpha-1) f(x) =  \sum w_i f_i
+	 * and in the second part,
+	 * int (1-x)^(beta-1) f(x) = \sum w_i f_i
+	 * Thereby avoiding the potential singularities with alpha<1 or beta<1.
+	 * 
+	 * @param X
+	 * @param F
+	 * @param alpha
+	 * @param beta
+	 * @return
+	 */
+	private double betaDensityQuadrature(double[] X, double[] F, double alpha, double beta) {
+		//X = [X0,X1,...,XN], X0 = 0, XN = 1
+		double Bab = Math.exp(GammaFunction.lnGamma(alpha) + GammaFunction.lnGamma(beta) - GammaFunction.lnGamma(alpha+beta));
+		int N = F.length-1;
+		int[] Ns;
+		int K = 7;
+
+		//Divide points into blocks of size K=7.
+		if (N<=K) {
+			Ns = new int[2];
+			Ns[0] = 0; Ns[1] = N;
+		} else {
+			int m = N/K;
+			int r = N%K;
+			if (r<=0) {
+				Ns = new int[m+1];
+				for(int i=0;i<m;i++)
+					Ns[i]=K*i;
+				Ns[m] = N;
+			} else {
+				Ns = new int[m+2];
+				for(int i=0;i<m/2;i++)
+					Ns[i]=K*i;
+				Ns[m/2]=K*(m/2)+r;
+				for(int i=m/2+1;i<=m;i++)
+					Ns[i]=K*i+r;
+			}
+		}
+
+		double total = 0.0;
+
+		int m = Ns.length-1;
+		if (m/2>0) {
+			for (int i=0;i<m/2;i++) {
+				double[] Xlocal = new double[Ns[i+1]-Ns[i]+2];
+				double[] Flocal = new double[Ns[i+1]-Ns[i]+2];
+				for (int j=Ns[i];j<=Ns[i+1];j++) {
+					double x = X[j];
+					double f = F[j];
+					double newf = f*Math.pow(1-x, beta-1)/Bab;
+					Xlocal[j-Ns[i]] = x;
+					Flocal[j-Ns[i]] = newf;
+					total = total+alphaKernelQuadrature(Xlocal,Flocal,alpha);
+				}
+			}  		
+		}
+		for (int i=m/2;i<=m-1;i++) {
+			double[] Xlocal = new double[Ns[i+1]-Ns[i]+2];
+			double[] Flocal = new double[Ns[i+1]-Ns[i]+2];
+			for (int j=Ns[i];j<=Ns[i+1];j++) {
+				double x = X[j];
+				double f = F[j];
+				double newf = f*Math.pow(x, alpha-1)/Bab;
+				Xlocal[j-Ns[i]] = x;
+				Flocal[j-Ns[i]] = newf;
+				total = total+betaKernelQuadrature(Xlocal,Flocal,beta);
+			}
+		}
+
+		return total;
+	}
+
+
+
+
+	int traverse(final Node node) {
+		int update = (node.isDirty() | hasDirt| Tree.IS_DIRTY);
+
+		final int iNode = node.getNr();
+
+
+		// If the node is internal, update the partial likelihoods.
+		if (!node.isLeaf()) {
+
+			// Traverse down the two child nodes
+			final Node child1 = node.getLeft(); //Two children
+			final int update1 = traverse(child1);
+
+			final Node child2 = node.getRight();
+			final int update2 = traverse(child2);
+
+			// If either child node was updated then update this node too
+			if (update1 != Tree.IS_CLEAN || update2 != Tree.IS_CLEAN) {
+
+				final int childNum1 = child1.getNr();
+				final int childNum2 = child2.getNr();
+
+				update |= (update1 | update2);
+
+				calculatePartials(childNum1, childNum2, node);
+
+				if (node.isRoot()) {     
+					double l = 0.0;
+					for (int s=0;s<nPoints;s++)
+						l += partialIntegral[iNode][s];
+					patternLogLikelihoods[thisPattern] = Math.log(l);              	
+				}
+
+			}
+		} else {
+			//Get the data for this node, and compute the partial likelihoods.
+			calculateLeafPartials(node);
+		}
+		return update;
+	}
+
+
+
 
 	// for each pattern
 	// calculate partials for node iNode given the partials/states of children childNum1 and childNum2
@@ -182,63 +364,60 @@ public class BetaApproximationLikelihood extends GenericTreeLikelihood {
 		double[] xvals = new double[nPoints];
 		double[] fvals = new double[nPoints];
 		int iNode = node.getNr();
-		
+
 		for (int j=0;j<nPoints;j++)
 			xvals[j] = (1.0/(nPoints-1))*j; //TODO: Use GaussQ points
-		
+
 		for (int s=0;s<nPoints;s++) {
 			if (node.isRoot()) {
 				for (int j=0;j<nPoints;j++) {					
-					fvals[j] = rootProb(xvals[j]) * partialIntegral[childNum1][j]*partialIntegral[childNum2][j];
+					fvals[j] = partialIntegral[childNum1][j]*partialIntegral[childNum2][j];
 				}
 			} else {		
-				for (int j=0;j<nPoints;j++) {			
-					fvals[j] = transProb(xvals[s],xvals[j],node.getLength()) * partialIntegral[childNum1][j]*partialIntegral[childNum2][j];
+				for (int j=0;j<nPoints;j++) {
+					fvals[j] = partialIntegral[childNum1][j]*partialIntegral[childNum2][j];
 				}
 			}
-			partialIntegral[iNode][s] = simpsons(xvals,fvals); 
+
+			double alpha,beta;
+			if (node.isRoot()) {
+				alpha = rootTheta/(1-2.0*rootTheta);
+				beta = alpha;
+			} else {
+				double coalRate=coalescenceRate.getValue(node.getNr());
+				double delta = 1.0 - Math.exp(-2.0*coalRate * node.getLength());
+				alpha = xvals[s]*(1-delta)/delta;
+				beta = (1-xvals[s])*(1-delta)/delta;
+			}
+
+			partialIntegral[iNode][s] = betaDensityQuadrature(xvals,fvals,alpha,beta); 
 		}			
 	}
-	
+
 	private void calculateLeafPartials(Node node) {
 		double[] xvals = new double[nPoints];
 		double[] fvals = new double[nPoints];
-		double[] lvals = new double[nPoints]; //Stores probability of data given leaf value.
-				
+
 		int n = lineageCounts[node.getNr()];
 		int r = redCounts[node.getNr()];
-		
-		
+
 		for (int j=0;j<nPoints;j++)
 			xvals[j] = (1.0/(nPoints-1))*j; //TODO: Use GaussQ points
-		
+
 		double logBinom = Binomial.logChoose(n, r);
 		for (int j=0;j<nPoints;j++) {
-			lvals[j] = Math.exp(logBinom + r*xvals[j] + (n-r)*(1.0-xvals[j]));
+			fvals[j] = Math.exp(logBinom + r*xvals[j] + (n-r)*(1.0-xvals[j]));
 		}
 		for (int s = 0;s<nPoints;s++) {
-			for (int j=0;j<nPoints;j++) {
-				fvals[j] = transProb(xvals[s],xvals[j],node.getLength())*lvals[j];
-			}
+			double coalRate=coalescenceRate.getValue(node.getNr());
+			double delta = 1.0 - Math.exp(-2.0*coalRate * node.getLength());
+			double alpha = xvals[s]*(1-delta)/delta;
+			double beta = (1-xvals[s])*(1-delta)/delta;
+			partialIntegral[node.getNr()][s] = betaDensityQuadrature(xvals,fvals,alpha,beta);
 		}
 	}
-	
-	private double betaDensity(double x, double alpha, double beta) {
-		double logP = GammaFunction.lnGamma(alpha+beta) - GammaFunction.lnGamma(alpha) - GammaFunction.lnGamma(beta);
-		logP += (alpha - 1)*Math.log(x) + (beta-1)*Math.log(1-x);
-		return Math.exp(logP);
-	}
-		
-	private double transProb(double x,double y, double t) {
-		//From Siren et al 2011. Compute density of y, for a Beta with parameters x*(1-t)/t, (1-x)(1-t)/t.
-		if (t==0)
-			return (x==y)?1.0:0.0;		
-		return betaDensity(y,x*(1-t)/t,(1-x)*(1-t)/t);
-	}
-	
-	private double rootProb(double x) {
-		return betaDensity(x,rootTheta,rootTheta);
-	}
-	
-	
+
+
+
+
 }
