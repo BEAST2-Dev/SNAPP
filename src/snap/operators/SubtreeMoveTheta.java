41a46835bb4ec9e62bbe255cd03d8b7353536a50
davidjamesbryant <david.bryant@otago.ac.nz>
2019-08-14 10:15:27 +1200

    Adding new operator - scales population sizes within subtree of species tree

---
+++ src/snap/operators/SubtreeMoveTheta.java
@@ -0,0 +1,86 @@
+
+/*
+ * File GammaMoveSubtree.java
+ *
+ * Copyright (C) 2010 Remco Bouckaert, David Bryant remco@cs.auckland.ac.nz
+ *
+ * This file is part of SnAP.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * SnAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *  SnAP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SnAP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+package snap.operators;
+
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.Operator;
+import beast.evolution.tree.Node;
+import beast.evolution.tree.Tree;
+import beast.core.parameter.RealParameter;
+import beast.util.Randomizer;
+
+@Description("Scales all theta values in a subtree by the same amount")
+public class SubtreeMoveTheta extends Operator {
+	public Input<Tree> m_pTree = new Input<Tree>("tree", "tree with phylogenetic relations");
+	public Input<RealParameter> m_coalescenceRate = new Input<RealParameter>("coalescenceRate", "population sizes");
+	public Input<Double> m_pScale = new Input<Double>("scale", "scale of move");
+
+	double m_fScale;
+
+	@Override
+	public void initAndValidate() {
+		m_fScale = m_pScale.get();
+	}
+	
+	@Override
+	public double proposal() {
+
+		Tree tree = m_pTree.get(this); //Species tree
+		Node [] nodes = tree.getNodesAsArray();
+		int nnodes = nodes.length; //Number of nodes in the species tree
+		int subtreeRootId = Randomizer.nextInt(nnodes);
+		Node subtreeRoot = nodes[subtreeRootId];
+		
+		RealParameter coalescenceRates = m_coalescenceRate.get(this);
+		double scale = Math.exp(m_fScale*(2.0*Randomizer.nextDouble() - 1.0));
+		
+		int numNodes = scaleSubtreeRates(subtreeRoot);
+		
+		return numNodes * Math.log(scale); //Hastings ratio
+	}
+ 
+ 	private int scaleSubtreeRates(Node v) {
+ 		int nNodes = 1;
+ 		if (!v.isLeaf()) {
+ 			nNodes+=scaleSubtreeRates(v.getLeft());
+ 			nNodes+=scaleSubtreeRates(v.getRight());
+ 		}
+ 		coalescenceRates.setValue(v.getNr(), coalescenceRates.getValue(v.getNr())*scale);
+ 		return nNodes;
+ 	}
+ 	
+ 	
+	/** automatic parameter tuning **/
+	@Override
+	public void optimize(double logAlpha) {
+		Double fDelta = calcDelta(logAlpha);
+		fDelta += Math.log(m_fScale);
+		m_fScale = Math.exp(fDelta);
+    }
+	
+} // class GammaMover 
