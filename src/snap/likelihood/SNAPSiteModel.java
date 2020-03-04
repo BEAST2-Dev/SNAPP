4201868d8f7bff52c349e165c3449d8ccd8343c4
rbouckaert <higgs.ml@gmail.com>
2020-03-05 07:30:25 +1300

    checkpoint

---
+++ src/snap/likelihood/SNAPSiteModel.java
@@ -0,0 +1,8 @@
+package snap.likelihood;
+
+import beast.evolution.sitemodel.SiteModel;
+
+/** container to make BEAUti use the SNAPSiteModelInputEditor**/
+public class SNAPSiteModel extends SiteModel {
+
+}
