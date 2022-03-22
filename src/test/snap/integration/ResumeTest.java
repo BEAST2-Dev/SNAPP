97c4b5aa6fd911c9085523090bc797a83b0c0979
rbouckaert <higgs.ml@gmail.com>
2022-03-23 11:44:01 +1300

    and another test bug

--- src/test/snap/integration/ResumeTest.java
+++ src/test/snap/integration/ResumeTest.java
@@ -32,7 +32,7 @@ public class ResumeTest  extends TestCase {
 		if (runable instanceof MCMC) {
 			MCMC mcmc = (MCMC) runable;
 			mcmc.setInputValue("preBurnin", 0);
-			mcmc.setInputValue("chainLength", 1000);
+			mcmc.setInputValue("chainLength", 1000l);
 			mcmc.run();
 		}
 		System.out.println("Done " + sFileName);
@@ -45,7 +45,7 @@ public class ResumeTest  extends TestCase {
 		if (runable instanceof MCMC) {
 			MCMC mcmc = (MCMC) runable;
 			mcmc.setInputValue("preBurnin", 0);
-			mcmc.setInputValue("chainLength", 1000);
+			mcmc.setInputValue("chainLength", 1000l);
 			mcmc.run();
 		}
 		System.out.println("Done " + sFileName);
